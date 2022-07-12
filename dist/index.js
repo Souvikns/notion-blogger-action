219344a1297428162613d27dc88c25ea2599b6a3
souvik <souvikde.ns@gmail.com>
2022-07-12 09:37:46 +0530

    chore: new build

--- dist/index.js
+++ dist/index.js
@@ -45,6 +45,7 @@ async function run() {
     console.log('LOADING CREDENTIALS');
     console.log('NOTION API KEY: ', notionApiKey !== undefined);
     console.log('NOTION DATABASE ID: ', notionDatabase !== undefined);
+    console.log('SERVICE_API_KEYS: ', serviceApiKeys);
     //await notionBlogger.publish(JSON.parse(serviceApiKeys));
 }
 run().then().catch(e => {
